077afded79c72525e92c05778dacd4827fe8ef8f
Benjamin Rowell <brrowell@gmail.com>
2019-04-22 16:44:09 +0100

    Restructuring and change over to Spotipy

--- spotty/spotty.py
+++ spotty/spotty.py
@@ -1,140 +1,79 @@
 # coding=utf-8
 import praw
 import re
-import spotify as sp
-import getpass
-import threading
-import os
-import sys
-import platform
+import spotipy as sp
 import requests
 import time
 import datetime
 import click
+import spotipy.util as util
+import os
 
-class PlaylistBuilder:
-    def __init__(self, sub_reddit, key_location=""):
+# Spotify Creds ###################################################
+# Environment variables should be set for the following:
+# SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
+###################################################################
 
-        self._sub_reddit = sub_reddit
-        self._playlist = None
-        self._key_location = key_location
-        self._scraped_tracks = []
-        self._potential_tracks = None
-        self._session = None
+# Reddit Creds ####################################################
+REDDIT_CLIENT_ID = os.environ["REDDIT_CLIENT_ID"]
+REDDIT_CLIENT_SECRET = os.environ["REDDIT_CLIENT_SECRET"]
+###################################################################
 
-    @property
-    def keylocation(self):
-        return self._keylocation
 
-    @keylocation.setter
-    def keylocation(self, value):
-        self._keylocation = value
+class SubredditPlaylistBuilder:
+    """
+    Builds a Spotify playlist by scraping a given subreddit for "artist - track" pattern in submission titles.
+    """
+    def __init__(self,
+                 spotify_username,
+                 subreddit="listentothis"):
 
-    @property
-    def playlist(self):
-        if self._playlist is None:
-            print('A playlist is yet to be created.')
-        return self._playlist
+        self.spotify_username = spotify_username
+        self.subreddit = subreddit
 
-    @property
-    def sub_reddit(self):
-        return self._sub_reddit
+        # Build client sessions
+        self.spotify = self._create_spotify_session()
 
-    def coroutine(func):
-        def start(*args, **kwargs):
-            cr = func(*args, **kwargs)
-            next(cr)
-            return cr
+        self.reddit = self._create_reddit_session()
 
-        return start
+        # Scrape tracks
+        self.scraped_tracks = self.scrape_subreddit()
 
-    def os_default(self, _platform):
+        # Add tracks
+        self.playlist_name = None
+        self.playlist_id = None
+        self.add_tracks_to_playlist()
+
+    def _create_spotify_session(self):
         """
-        Helper returning default locations for OSes.
+        Uses environment vars for Client ID, Secret and Redirect URI to create a Spotify
+        session.
+
+        :return: Instance of Spotify Session.
         """
-        return {
-            'Darwin': '/Users/%s/spotify_appkey.key' % (os.getlogin()),
-            'Linux': '/home/%s/spotify_appkey.key' % (os.getlogin()),
-        }[_platform]
-
-    def key_location_setup(self):
-        """
-        Method to locate and setup a users Spotify key. Location is defaulted to
-        a user's home directory, unless specified otherwise using the positional
-        argument 'keyfile'.
-
-        Default Locations:
-
-        Mac:
-                /Users/*USER*/spotify_appkey.key
-
-        Linux:
-                /home/*USER*/spotify_appkey.key
-        """
-
-        if not self._key_location:
-            config = sp.Config()
-            config.user_agent = 'Spotty v0.1'
-            self._key_location = self.os_default(platform.system())
-            config.load_application_key_file(filename=self._key_location)
-            return config
-
-        elif self._key_location:
-            config = sp.Config()
-            config.user_agent = 'Spotty v0.1'
-            config.load_application_key_file(filename=self._key_location)
-            return config
-
-        else:
-            print("You Entered an invalid response, try again.")
-            key_location_setup()
-
-    def session_init(self):
-        """
-        Method to initialise a session with Spotify using credentials input
-        by the user and a spotify_appkey file, which is passed to the session
-        configuration as the new Spotify session object is initialised.
-        """
-
+        scope = "playlist-modify-private"
         try:
+            token = util.prompt_for_user_token(self.spotify_username, scope)
 
-            logged_in_event = threading.Event()
-
-            def connection_state_listener(_session):
-                """
-                Helper function to awaken waiting threads.
-                """
-                if session.connection.state is sp.ConnectionState.LOGGED_IN:
-                    logged_in_event.set()
-
-            session = sp.Session(config=self.key_location_setup())
-            loop = sp.EventLoop(session)
-            loop.start()
-            session.on(sp.SessionEvent.CONNECTION_STATE_UPDATED,
-                       connection_state_listener)
-            if sys.version_info.major == 2:
-                username = raw_input('Username: ')
-            elif sys.version_info.major == 3:
-                username = input('Username: ')
-            password = getpass.getpass('Password: ')
-            session.login(username, password, remember_me=True)
-            logged_in_event.wait()
-            self._session = session
-
+            if token:
+                print("Successfully established Spotify session.")
+                return sp.Spotify(auth=token)
+            else:
+                raise ValueError("No token")
         except Exception as e:
+            raise e
 
-            print(e, e.args)
-
-    def reddit_connection(self):
+    def _create_reddit_session(self):
         """
-        Method for establishing a connection to Reddit using the PRAW library,
-        returns a session object.
+        Uses environment vars for Client ID and Secret to create a Reddit session.
+
+        :return: Instance of Reddit Session.
         """
 
         try:
-            r = praw.Reddit(user_agent='Spotty v0.1')
-            return r
-
+            return praw.Reddit(client_id=REDDIT_CLIENT_ID,
+                               client_secret=REDDIT_CLIENT_SECRET,
+                               user_agent='Spotty v0.1')
         except requests.HTTPError as e:
             if e.errno in [429, 500, 502, 503, 504]:
                 print("Reddit is down (error %s), sleeping..." % e.errno)
@@ -142,81 +81,77 @@ class PlaylistBuilder:
                 pass
             else:
                 raise e
-
         except Exception as e:
             print("couldn't Reddit: %s" % str(e))
 
-    def reddit_scrape(self, session):
+    def scrape_subreddit(self):
         """
-        Method for scraping all potential tracks from a subreddit. This is
-        currently defaulted to 'listentothis' and scrapes top posts from the
-        last week. All done using the praw package. Submissions are sent to
-        the track_validator coroutine.
+        Method for scraping all potential tracks from a subreddit. Submission titles
+        are validated using an "artist - title" regex in track_validator.
         """
+        tracks = []
 
-        t = self.track_validator()
+        def track_validator(submission):
+            artist_regex = re.compile('(\w.+) \-\-?')
+            title_regex = re.compile('\w.+ --? (\w.+) \[')
 
-        for submission in session.get_subreddit(
-            self._sub_reddit).get_top_from_week(limit=100):
-            t.send(submission)
-        t.close()
+            try:
+                if re.match(artist_regex, submission.title):
+                    artist = re.match(artist_regex, submission.title).group(1)
+                    if re.match(title_regex, submission.title):
+                        title = re.match(title_regex, submission.title).group(1)
+                        return artist, title
+                return False
+            except Exception as e:
+                print(e)
+                return False
 
-    @coroutine
-    def track_validator(self):
-        """
-        Check for title - artist structure in titles scraped from subreddit,
-        sending valid ones to add_tracks_to_playlist coroutine.
-        """
-        attp = self.add_tracks_to_playlist()
-        artistRegex = re.compile('\d{0,5} :: (\w.+) \-\-?')
-        titleRegex = re.compile('\w.+ --? (\w.+) \[')
-        try:
-            while True:
-                submission = (yield)
-                if re.match(artistRegex, str(submission)):
-                    artist = re.match(artistRegex, str(submission))
-                    if re.match(titleRegex, str(submission)):
-                        title = re.match(titleRegex, str(submission))
-                        attp.send((artist, title))
+        for submission in self.reddit.subreddit(self.subreddit).top('week'):
+            try:
+                tracks.append(track_validator(submission))
+            except Exception as e:
+                print(e)
 
-        except GeneratorExit:
-            attp.close()
+        return tracks
 
-    @coroutine
     def add_tracks_to_playlist(self):
         """
-        Takes artist - title tuples sent from track_validator coroutine and
-        searches for existence on Spotify. If they exist, they are added to
-        the playlist.
+        Checks scraped tracks from Reddit exist in Spotify, builds a playlist and adds tracks.
         """
+
+        spotify_tracks = []
 
         try:
-            while True:
-                track = (yield)
-                searchtrack = self._session.search(
-                    'artist:"%s" title:"%s"' %
-                    (track[0].group(1), track[1].group(1))).load()
-                if len(searchtrack.tracks) >= 1:
-                    print(searchtrack.tracks[0].name + ' - ' +
-                          searchtrack.tracks[0].artists[0].load().name)
-                    self._playlist.add_tracks(searchtrack.tracks[0])
+            for track in self.scraped_tracks:
+                if track:
+                    searchtrack = self.spotify.search(
+                        '%s %s' %
+                        (track[0], track[1]))
 
-        except GeneratorExit:
-            print("=== Done ===")
+                    if searchtrack["tracks"]["items"]:
+                        search_result_artist = searchtrack["tracks"]["items"][0]["artists"][0]["name"]
+                        search_result_track = searchtrack["tracks"]["items"][0]["name"]
 
-    def build_playlist_container(self):
-        """
-        Use scraped tracks from subreddit to create a playlist.
-        """
-        # Create new playlist with subreddit as title
-        built_container = self._session.playlist_container
-        built_container.load()
-        playlist = built_container.add_new_playlist(
-            self.sub_reddit + " - " + str(datetime.date.today()))
-        self._playlist = playlist
+                        print(f"Reddit  : {track[0]} - {track[1]}")
+                        print(f"Spotify : {search_result_artist} - {search_result_track}")
+                        print("---------------------------------------")
+                        spotify_tracks.append(searchtrack["tracks"]["items"][0]["id"])
+
+            if spotify_tracks:
+                # Create new playlist with subreddit as title
+                user_id = self.spotify.me()["id"]
+                self.playlist_name = self.subreddit + " - " + str(datetime.date.today())
+                self.playlist_id = self.spotify.user_playlist_create(user_id, self.playlist_name, public=False)['id']
+
+            self.spotify.user_playlist_add_tracks(self.spotify.me()['id'],
+                                                  self.playlist_id,
+                                                  spotify_tracks)
+
+        except Exception as e:
+            print(e)
 
 
-def main():
+if __name__ == "__main__":
     print('''
 
 
@@ -238,36 +173,16 @@ def main():
 
     You must have a premium Spotify membership to continue.
 
-    You will also need your API token found here:
+    You will also need to grant Spotty access to build your playlist.
 
-        http://devaccount.spotify.com/my-account/keys/
-
-    Download your key and store it in your user directory!
+    Accept in your browser and paste the redirect URL in the terminal. 
 
     ''')
 
-    def build_new_playlist(new_playlist):
-        new_playlist.key_location_setup()
-        new_playlist.session_init()
-        new_playlist.build_playlist_container()
-        new_playlist.reddit_scrape(new_playlist.reddit_connection())
 
     @click.command()
-    @click.argument('keyfile', required=False, nargs=1, type=click.Path(exists=True))
-    def parse_args(keyfile):
-        if keyfile:
-            click.echo("Keyfile: " + keyfile)
-            build_new_playlist(PlaylistBuilder('listentothis', keyfile))
-        else:
-            if click.confirm('Did you mean to use the default key location?'):
-                click.echo('You may continue...\n')
-                build_new_playlist(PlaylistBuilder('listentothis'))
-            else:
-                keyfile = click.prompt('Please enter a keyfile location', type=click.Path(exists=True))
-                build_new_playlist(PlaylistBuilder('listentothis', keyfile))
+    @click.argument('username', required=True, nargs=1, type=click.STRING)
+    def parse_args(username):
+        SubredditPlaylistBuilder(username)
 
     parse_args()
-
-
-if __name__ == "__main__":
-    main()
